95a091d3c46e35b145e79d0485b8405e1ab80e10
gerpag <germinalfp@gmail.com>
2024-06-20 19:05:12 +0200

    styles on AboutMe

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,4 +1,73 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../css/ProjectsSection.css';
+
+import arrowRightIcon from '../assets/icons/arrow-right.svg';
+import fastDeliveryImage from '../assets/images/FastDelivery.png';
+import polirubroImage from '../assets/images/polirubro.png';
+import houseOfDevImage from '../assets/images/HouseOfDev.png';
+import moreImage from '../assets/images/plano-MORE.jpeg';
+
+interface Project {
+  title: string;
+  description: string;
+  image: string;
+  link: string;
+}
+
+const projects: Project[] = [
+  {
+    title: 'Delivery App',
+    description: 'Elit pellentesque habitant morbi tristique senectus et netus et. Elementum tempus egestas sed sed risus pretium quam. Nibh ipsum consequat nisl vel pretium',
+    image: fastDeliveryImage,
+    link: '/project/delivery-app'
+  },
+  {
+    title: 'Digital Catalog',
+    description: 'Elit pellentesque habitant morbi tristique senectus et netus et. Elementum tempus egestas sed sed risus pretium quam. Nibh ipsum consequat nisl vel pretium',
+    image: polirubroImage,
+    link: '/project/digital-catalog'
+  },
+  {
+    title: 'Real-estate App',
+    description: 'Elit pellentesque habitant morbi tristique senectus et netus et. Elementum tempus egestas sed sed risus pretium quam. Nibh ipsum consequat nisl vel pretium',
+    image: houseOfDevImage,
+    link: '/project/real-estate-app'
+  },
+  {
+    title: 'More Projects',
+    description: 'Elit pellentesque habitant morbi tristique senectus et netus et. Elementum tempus egestas sed sed risus pretium quam. Nibh ipsum consequat nisl vel pretium',
+    image: moreImage,
+    link: '/project/more-projects'
+  }
+];
+
+const ProjectsSection: React.FC = () => (
+  <div id="my-work-section">
+    <h2 className="section-title">My Projects</h2>
+    <div className="projects-container">
+      {projects.map((project, index) => (
+        <div key={index} className="project-card">
+          <div className="project-card-overlay">
+            <h3 className="project-title">{project.title}</h3>
+            <Link className="button" to={project.link}>
+              <span className="button-text">Read More</span>
+              <img src={arrowRightIcon} alt="Arrow Right" className="right-arrow-icon" />
+            </Link>
+          </div>
+          <img src={project.image} alt={project.title} className="project-image" />
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
+export default ProjectsSection;
+
+
+
+
+/* import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Modal from '../commons/Modal';
 import '../css/ProjectsSection.css';
@@ -100,6 +169,6 @@ const ProjectsSection: React.FC = () => {
   );
 };
 
-export default ProjectsSection;
+export default ProjectsSection; */
 
 
